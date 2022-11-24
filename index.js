f7f36079b51e405ab2eb20b97579a8aebb2f10ed
Sirumba <simonsmaina45@gmail.com>
2022-11-24 14:27:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let ride;
+   if (someValue <= 400) {
+     ride = "This one is on me!";
+   } else if (someValue > 2000) {
+     ride = "I will gladly take your thirty bucks."; 
+   } if (someValue > 2500){
+     ride = "No can do.";}
+  return ride
+   }
+scuberGreetingForFeet (199)
+scuberGreetingForFeet (2001)
+scuberGreetingForFeet (2501)
+
+function ternaryCheckCity(city){
+  return city === "NYC"? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ternaryCheckCity(true)
+ternaryCheckCity (false)
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip)  {
+    case "generous":
+  return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default: 
+    return "Bye."
+     break
+  }
+  }
